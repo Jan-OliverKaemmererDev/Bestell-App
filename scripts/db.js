0327f98309a1715ee9143189cdefe942a0f4d285
Jan-Oliver <jowieja22@gmail.com>
2025-12-20 16:58:30 +0100

    added font and db.js

---
+++ scripts/db.js
@@ -0,0 +1,28 @@
+let myMeals = [
+    {
+        "category": "Burger & Sandwiches",
+        "meals": [
+            {"name": "Veggie mushroom black burger", "description": "Mixed green salad, Tomatoes, Edamame", "price": 16.90},
+            {"name": "All meat burger", "description": "Beef, Bacon, Dill pickles, Smoked cheese", "price": 15.90},
+            {"name": "Beef red burger", "description": "Beef, Cheese, Tomatoes, Lettuce, Onion", "price": 14.90}
+        ]
+    },
+    {
+        "category": "Pizza (30cm)",
+        "meals": [
+            {"name": "Pizza Margherita", "description": "Tomato Sauce, Mozzarella", "price": 11.90},
+            {"name": "Pizza Chorizo", "description": "Tomato slices, Mozzarella, Chorizo", "price": 13.90},
+            {"name": "Funghi", "description": "Red onion, Olives, Button Mushrooms", "price": 12.90}
+        ]
+    },
+    {
+        "category": "Salad",
+        "meals": [
+            {"name": "Warm beef arugula salad", "description": "Beef, Arugula, Field salad, Greek feta", "price": 16.90},
+            {"name": "Mini green salad", "description": "Green salad, Cucumber, Carrots, Parsley", "price": 7.90},
+            {"name": "Green Salad with sea food", "description": "Mixed greens, Cherry tomatoes, Red onion", "price": 16.90}
+        ]
+    }
+];
+
+let basket = [];
